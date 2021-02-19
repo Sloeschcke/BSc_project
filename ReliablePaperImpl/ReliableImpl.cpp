87e65b2f6587e35b0b069d9dbcb8ed89e6b57c37
mabeto5p <mabeto5p@live.dk>
2021-02-19 10:52:24 +0100

    Initial relativePath, but does it work on mac?

--- ReliablePaperImpl/ReliableImpl.cpp
+++ ReliablePaperImpl/ReliableImpl.cpp
@@ -3,15 +3,39 @@
 #include "hoved.h"
 #include "pruner.h"
 #include "tests.h"
+#include <Windows.h>
+
 using namespace std;
+
+// string GetPath2()
+// {
+//      wchar_t buffer[MAX_PATH] = {};
+//      ::GetSystemDirectoryA(buffer,_countof(buffer)); // notice the A
+//      strcat(buffer,"\\version.dll");
+
+//      return std::string(buffer);
+//  }
+//  #include <string>
+// #include <windows.h>
+
+std::string getexepath()
+{
+  char result[ MAX_PATH ]={};
+  return std::string( result, GetCurrentDirectoryA( MAX_PATH, result) );
+}
 
 int main(int argc, char const *argv[])
 {
-    string graph_file = "C:\\Users\\mabet\\OneDrive - Aarhus Universitet\\Datalogi\\Bachelor projekt\\BSc_project\\ReliablePaperImpl\\graph_file.inf";
+    // string graph_file = "C:\\Users\\mabet\\OneDrive - Aarhus Universitet\\Datalogi\\Bachelor projekt\\BSc_project\\ReliablePaperImpl\\graph_file.inf";
+    // string graph_file = +"\\ReliablePaperImpl\\graph_file.inf";
 	std::cout << "Hello ze world";
+	string c = getexepath();
+	string graph_file = c+"\\ReliablePaperImpl\\graph_file.inf";
+	std::cout << c;
 	Graph graph (7, graph_file);
 	graph.readGraph();
 	std::cout << "Hello ze world";
 	testAll();
 	return 0;
 }
+
