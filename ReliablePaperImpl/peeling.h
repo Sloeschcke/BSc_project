1b3046ab9bdb295279516591403b359f7a5f0a82
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-03-12 13:58:50 +0100

    done

--- ReliablePaperImpl/peeling.h
+++ ReliablePaperImpl/peeling.h
@@ -17,6 +17,14 @@ bool containsSupersetOfElem(set<set<int>> MFCS, set<int> m){
     return true;
 }
 
+void checkReliabilityInGraphSamples2(set<set<int>>& MFCS, set<set<int>>& MFCS2, vector<vector<vector<int>>>& graphSamples2, long double threshold){
+    for(auto m : MFCS){
+        if(subgraphReliability(graphSamples2, m) >= threshold){
+            MFCS2.insert(m);
+        }
+    }
+}
+
 void peeling(vector<vector<vector<int>>> graphSamples, set<set<int>> mfls, double threshold, set<set<int>> *MFCS, int numSamples){
     for (auto m : mfls){
         if(subgraphReliability(graphSamples, m) >= threshold){ //check if m is a frequent cohesive set
@@ -34,16 +42,20 @@ void peeling(vector<vector<vector<int>>> graphSamples, set<set<int>> mfls, doubl
 }
 
 
-set<set<int>> runPeeling(string fileName, int numNodes, int numEdges, int numSamples, long double threshold){
+set<set<int>> runPeeling(string fileName, int numNodes, int numEdges, int numSamples, long double threshold, long double eps){
     Graph graph (numNodes, numEdges, fileName);
 	graph.readGraph();
     vector<vector<vector<int>>> graphSamples =  sample(graph, numSamples);
+    vector<vector<vector<int>>> graphSamples2 = sample(graph, numSamples);
     vector<vector<int>> components = connectedComponents(&graphSamples);
     
-    set<set<int>> maximalFI = getMFI(components, threshold, numSamples);
+    set<set<int>> maximalFI = getMFI(components, threshold-eps, numSamples);
     set<set<int>> MFCS;
     peeling(graphSamples, maximalFI, threshold, &MFCS, numSamples);
-    return MFCS;
+
+    set<set<int>> MFCS2;
+    checkReliabilityInGraphSamples2(MFCS, MFCS2, graphSamples2, threshold+eps);
+    return MFCS2;
 }
 
 #endif
