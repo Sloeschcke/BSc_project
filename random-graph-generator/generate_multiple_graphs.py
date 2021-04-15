9b92d062065f88360db5320f9802c520ff7f2e4b
mabeto5p <mabeto5p@live.dk>
2021-04-15 09:39:15 +0200

    new files

--- random-graph-generator/generate_multiple_graphs.py
+++ random-graph-generator/generate_multiple_graphs.py
@@ -1,70 +1,79 @@
 
 import os
 import random
+from pathlib import Path
 
 
 def main():
-    relative_path = "./generated_graphs/"
-    value_for_plot= "edge_degree"
-    # vertices = [40,80,160,240,320]
-    edges = [80,120,160,200,240,280,320]
-    for i in edges:
-        filename = relative_path+value_for_plot+str(i)
-        create_graph_file( 80, i, filename, seed=i)
-        # create_graph_file( i, i*2, filename, seed=i) #vertices
+    generate_edge_degree_graphs()
+    # generate_numNodes_graphs()
+    
+def get_relative_path(value_for_plot):
+    relative_path = Path("C:/Users/mabet/OneDrive - Aarhus Universitet/Datalogi/Bachelor projekt/BSc_project/GraphsGeneration/random-graph-generator")
+    print(os.path)
+    path = relative_path/value_for_plot
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
 
-        r = open(filename, "r")
-        first_line = r.readline()
+def generate_numNodes_graphs():
+    num_nodes = [10, 20, 40, 80, 160, 280, 560]
+    edge_degree = 2
+    value_for_plot = "num_nodes_edgedegree2"
+    path = get_relative_path(value_for_plot)
+    for i, nodes in enumerate(num_nodes):
+        edges = edge_degree*nodes
+        create_processed_graph_file(path, i, nodes, edges, 1, value_for_plot, nodes)
 
-        relative_path_processed = "./generated_graphs/processed_"
-        filename = relative_path_processed +value_for_plot+str(i)
-        w = open(filename, "w")
-        lines = r.readlines()[1:]
+    
+def generate_edge_degree_graphs():
+    value_for_plot = "edge_degree"
+    relative_path_processed = get_relative_path(value_for_plot)
+    nodes = 80
+    edges = [5]
+    # edges = [80,120,160,200,240,280,320]
+    for index,i in enumerate(edges):
+        value = i/nodes
+        create_processed_graph_file(relative_path_processed, index, nodes, i, 1, value_for_plot, value)
 
-        
-        lowest = 99999999
+def create_processed_graph_file(path, index, nodes, edges, seed, value_for_plot,value):
+    filename = str(index)
+    create_graph_file( nodes, edges, filename, seed=seed)
+    r = open(filename, "r")
+    first_line = r.readline()
+    
+    filename_processed = path/f"{str(index)}.txt"
+    w = open(filename_processed, "w+")
+    lines = r.readlines()[1:]
 
-        edgesSet = set()
+    
+    lowest = 99999999
 
-        for line in lines:
-            numbers = [int(n) for n in line.split()]
-            lowest = min(lowest, min(numbers))
-        random.seed(i) 
-        for line in lines:
-            numbers = [int(n) for n in line.split()]
-            numbers = sorted([x-lowest for x in numbers])
-            edgesSet.add(tuple(numbers))
+    edgesSet = set()
 
-        w.write(first_line.rstrip("\n") +" "+value_for_plot +"\n" )
-        a = open(filename, "a")
-        for node1, node2 in edgesSet:
-            editedLine = f"{node1} {node2} {str(random.uniform(0,1))}\n"
-            a.write(editedLine)
+    for line in lines:
+        numbers = [int(n) for n in line.split()]
+        lowest = min(lowest, min(numbers))
+    random.seed(seed) 
+    for line in lines:
+        numbers = [int(n) for n in line.split()]
+        numbers = sorted([x-lowest for x in numbers])
+        edgesSet.add(tuple(numbers))
+    
+    w.write(first_line.rstrip("\n") +" "+value_for_plot + " " + str(value))
+    a = open(filename_processed, "a")
+    for node1, node2 in edgesSet:
+        editedLine = f"{node1} {node2} {str(random.uniform(0,1))}\n"
+        a.write(editedLine)
 
+    w.close()
+    a.close()
+    r.close()
+    # os.remove(filename)
 
 def create_graph_file( nodes, edges, filename , seed):
-    command_nodes_and_edges = "python3 gen_graph.py -grnm -n " + str(nodes) + " -m " + str(edges) + " --seed "+ str(seed) + " --out " + filename
+    command_nodes_and_edges = "python gen_graph.py -grnm -n " + str(nodes) + " -m " + str(edges) + " --seed "+ str(seed) + " --out " + filename
     os.system(command_nodes_and_edges)
-
-
-
-def prepend_line(file_name, line):
-    """ Insert given string as a new line at the beginning of a file """
-    # define name of temporary dummy file
-    dummy_file = file_name + '.bak'
-    # open original file in read mode and dummy file in write mode
-    with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
-        # Write given line to the dummy file
-        write_obj.write(line + '\n')
-        # Read lines from original file one by one and append them to the dummy file
-        for line in read_obj:
-            write_obj.write(line)
-    # remove original file
-    os.remove(file_name)
-    # Rename dummy file as the original file
-    os.rename(dummy_file, file_name)
-
-
 
 if __name__ == "__main__":
     main()
