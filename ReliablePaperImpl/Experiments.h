5e18c283bc697138d27259b5974c8e69dcb73658
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-05-10 14:36:49 +0200

    experiments finished - wierd bug when doing edge_degree???

--- ReliablePaperImpl/Experiments.h
+++ ReliablePaperImpl/Experiments.h
@@ -9,7 +9,6 @@
 #include "TopKPeeling.h"
 #include "IterApriori.h"
 #include "naiveTopKPeeling.h"
-using namespace std;
 
 // string abspath = "C:\\Users\\mabet\\OneDrive - Aarhus Universitet\\Datalogi\\Bachelor projekt\\BSc_project\\GraphsGeneration\\processed_graphs\\";
 // string abspath = "/Users/sebastianloeschcke/Desktop/6.semester/BSc";
@@ -24,19 +23,17 @@ struct ValueTime {
     }
 };
 
+// C:\Users\chris\Documents\6. Semester\Bachelor Project\BSc_project\GraphsGeneration\processed_graphs\edge_degree\0\0.txt
 ValueTime runExperiment(string path, int numSamples, int k){
-    string numNodes, numEdges;
-    string problem;
-    string value;
     Graph graph = Graph(path);
-    cout << graph.getValue() << "\n";
+    string value = graph.getValue();
+    cout << value << "\n";
     vector<vector<vector<int>>> samples = sample(graph, numSamples);
     clock_t start;
     start = clock();
     vector<Candidate> result = runTopKPeelingWithoutSampling(samples, numSamples, k);
     double duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
     return ValueTime(duration, stod(value));
-
 }
 
 void writeListOfResultsToFile(string outPath, vector<ValueTime> results, string graphTitle, string xLabel){
@@ -50,7 +47,7 @@ void writeListOfResultsToFile(string outPath, vector<ValueTime> results, string 
     outFile.close();
 }
 
-void runExperiment(bool valency, string category){
+void runExperiments(bool valency, string category){
     string path = abs_path + category;
     vector<ValueTime> results;
     int numExperiments = 10;
@@ -62,19 +59,32 @@ void runExperiment(bool valency, string category){
         }
         for (int i = 0; i<numRepetitions; i++){
             string filePath = folderPath + "\\"+ to_string(i)+".txt";
-            ValueTime valTime = runExperiment(filePath, 100, 2);
-            cout << "finished value: "<< valTime.value << " in time: " << valTime.time <<"\n";
-            results.push_back(valTime);
+            double long value;
+            double long time;
+            if(category == "Varying K"){
+                ValueTime valTime = runExperiment(filePath, 100, i + 1);
+                results.push_back(valTime);
+                value = valTime.value;
+                time = valTime.time;
+            } else {
+                ValueTime valTime = runExperiment(filePath, 100, 2);
+                results.push_back(valTime);
+                value = valTime.value;
+                time = valTime.time;
+            }
+            cout << "finished value: "<< value << " in time: " << time <<"\n";
             cout << "============================\n";
             }
     }
+
     string resPath;
     if(valency){
         resPath =  path + "\\output\\results_valency.txt";
     } else {
         resPath = path + "\\output\\results.txt";
     }
-    writeListOfResultsToFile(path+"/output/results.txt", results, "Number of nodes", "Nodes");
+    writeListOfResultsToFile(resPath, results, category, category);
 }
+
 #endif
 
--- ReliablePaperImpl/ReliableImpl.cpp
+++ ReliablePaperImpl/ReliableImpl.cpp
@@ -2,16 +2,20 @@
 #include "graph.h"
 #include "hoved.h"
 #include "tests.h"
-#include <cstdio>
 #include "Experiments.h"
+#include <cstdio>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	string varyingK = "Varying K";
+	string numNodes = "num_nodes";
+	string edgeDegree = "edge_degree";
+
 	std::cout << "Hello ze world \n";
-	testAll();
-	// runExperiment(false, "num_nodes");
+	// testAll();
+	runExperiments(false, edgeDegree);
 	return 0;
 }
 	
