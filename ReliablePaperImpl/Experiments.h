afec5894d21348c02b3729dc3b568f6c33a2c2d6
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-05-28 09:50:52 +0200

    ready to run

--- ReliablePaperImpl/Experiments.h
+++ ReliablePaperImpl/Experiments.h
@@ -305,14 +305,14 @@ void runAllNaiveExperiments(){
     long double eps = 0.05;
     long double delta = 0.01;
     int k = 3;
-    runExperiments(false, "Naive", "edge_degree", eps, delta, k);
+    // runExperiments(false, "Naive", "edge_degree", eps, delta, k);
     runExperiments(true, "Naive", "edge_degree", eps, delta, k);
-    runExperiments(false, "Naive", "Varying_K", eps, delta, k);
-    runExperiments(true, "Naive", "Varying_K", eps, delta, k);
-    runExperiments(true, "Naive", "epsilon", eps, delta, k);
-    runExperiments(false, "Naive", "epsilon", eps, delta, k);
-    runExperiments(true, "Naive", "delta", eps, delta, k);
-    runExperiments(false, "Naive", "delta", eps, delta, k);
+    // runExperiments(false, "Naive", "Varying_K", eps, delta, k);
+    // runExperiments(true, "Naive", "Varying_K", eps, delta, k);
+    // runExperiments(true, "Naive", "epsilon", eps, delta, k);
+    // runExperiments(false, "Naive", "epsilon", eps, delta, k);
+    // runExperiments(true, "Naive", "delta", eps, delta, k);
+    // runExperiments(false, "Naive", "delta", eps, delta, k);
     runExperiments(true, "Naive", "num_nodes", eps, delta, k);
     runExperiments(false, "Naive", "num_nodes", eps, delta, k);
 }
