0c4e3b4e9f4b9e9ff009690498675ad9a6f6ab5e
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2021-06-02 11:30:44 +0200

    fdist

--- ReliablePaperImpl/ReliableImpl.cpp
+++ ReliablePaperImpl/ReliableImpl.cpp
@@ -12,9 +12,9 @@ int main(int argc, char const *argv[])
 	std::cout << "Hello ze world \n";
 	// string path = "C:\\Users\\chris\\Documents\\6. Semester\\Bachelor Project\\BSc_project\\GraphsGeneration\\processed_graphs\\Varying_K\\0\\1.txt";
 	// ValueTime valTime = run2StepExperiment(path, 2, 0.05, 0.01);
-	testAll();
+	// testAll();
 	// Precision2Step();
-	// runNumNodesFDist();
+	runNumNodesFDist();
 	// ToyDataSetValidation();
 	// run1StepAll();
 	// run2StepAll();
